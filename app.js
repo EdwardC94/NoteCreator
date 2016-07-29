3a2d975c01e320f92371b74af81ef211f01987a7
Eduardo <edwardc94@outlook.com>
2016-07-28 19:47:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,30 @@
 (function() {
 	var app = angular.module("NoteCreator", []);
-	var policyInformation = {
-		pn : "",
-		phn : "",
-		email : "",
-		dop : "",
-		doe : "",
-		amntRef : "",
-		cardBrand : "",
-		l4d : "",
-		availableOptions: availableOptions,
-		selectedOption: availableOptions[0] //This sets the default value of the select in the ui								
+	function Policy(availableOptions) = {
+		this.pn : "";
+		this.phn : "";
+		this.email : "";
+		this.dop : "";
+		this.doe : "";
+		this.amntRef : "";
+		this.cardBrand : "";
+		this.l4d : "";
+		this.availableOptions: availableOptions || [];
+		this.selectedOption: availableOptions[0] || null; //This sets the default value of the select in the ui								
 	};
-	
-	var availableOptions = [
-	    {id: '0', reason: 'Cancellation Reason', exp: ''},
-	    {id: '1', reason: 'Unintended Purchase', exp: 'the customer made the purchase of the Policy by mistake'},
-	    {id: '2', reason: 'Satisfaction Guarantee', exp: 'the customer is not satisfied with the coverage on the Policy and wants to cancel it'},
-	    {id: '3', reason: 'Duplicate', exp: 'the customer is covered by other insurance Policy'},
-	    {id: '4', reason: 'Did not pick up rental', exp: 'the reservation of the car was cancelled'},
-	    {id: '5', reason: 'AGA not accepted by Car Rental Agency', exp: 'the customer had to purchase insurance from a different Company, because the Car Rental Agency would not accept AGAs'}
-	    ];
 	app.controller("CancellationCtrl", ["$scope", function($scope){
-	    $scope.data = {
-			pn : "",
-			phn : "",
-			email : "",
-			dop : "",
-			doe : "",
-			amntRef : "",
-			cardBrand : "",
-			l4d : "",
-			availableOptions: availableOptions
-			selectedOption: availableOptions[0] //This sets the default value of the select in the ui								
-		};
+	
+    	var availableOptions = [
+    	    {id: '0', reason: 'Cancellation Reason', exp: ''},
+    	    {id: '1', reason: 'Unintended Purchase', exp: 'the customer made the purchase of the Policy by mistake'},
+    	    {id: '2', reason: 'Satisfaction Guarantee', exp: 'the customer is not satisfied with the coverage on the Policy and wants to cancel it'},
+    	    {id: '3', reason: 'Duplicate', exp: 'the customer is covered by other insurance Policy'},
+    	    {id: '4', reason: 'Did not pick up rental', exp: 'the reservation of the car was cancelled'},
+    	    {id: '5', reason: 'AGA not accepted by Car Rental Agency', exp: 'the customer had to purchase insurance from a different Company, because the Car Rental Agency would not accept AGAs'}
+    	    ];
+	    $scope.data = new Policy(availableOptions);
 		$scope.clear = function(){
-			$scope.data = defaultOptions;
+			$scope.data = new Policy(availableOptions);
 		};
 		$scope.copy = function() {
 			var urlField = document.querySelector('#canc_note');
