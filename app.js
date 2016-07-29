07dcf860218d4f5785c5110304e8c83494e7515b
Eduardo <edwardc94@outlook.com>
2016-07-29 17:37:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,6 +14,12 @@
 		this.availableOptions= availableOptions || [];
 		this.selectedOption= availableOptions[0] || null; //This sets the default value of the select in the ui	*/							
 	};
+	function Claim() {
+	    this.dateFiled = "";
+	    this.incDate = "";
+	    this.amountClaimed = "";
+	    this.addInfo = "";
+	}
 	function copy (selector) {
 		var urlField = document.querySelector(selector);
 		var range = document.createRange();  
@@ -23,7 +29,7 @@
 		window.getSelection().removeAllRanges();
 	};
 	app.controller("CancellationCtrl", ["$scope", function($scope){
-    	var availableOptions = [
+    	$scope.availableOptions = [
     	    {id: '0', reason: 'Cancellation Reason', exp: ''},
     	    {id: '1', reason: 'Unintended Purchase', exp: 'the customer made the purchase of the Policy by mistake'},
     	    {id: '2', reason: 'Satisfaction Guarantee', exp: 'the customer is not satisfied with the coverage on the Policy and wants to cancel it'},
@@ -31,6 +37,7 @@
     	    {id: '4', reason: 'Did not pick up rental', exp: 'the reservation of the car was cancelled'},
     	    {id: '5', reason: 'AGA not accepted by Car Rental Agency', exp: 'the customer had to purchase insurance from a different Company, because the Car Rental Agency would not accept AGAs'}
     	    ];
+	    $scope.selectedOption = $scope.availableOptions[0];
 	    $scope.data = new Policy();
 		$scope.clear = function(){
 			$scope.data = new Policy();
@@ -38,15 +45,5 @@
 		$scope.copy = function() {
 			copy('#canc_note');
 		};
-		/*
-		$scope.copy = function() {
-			var urlField = document.querySelector('#canc_note');
-			var range = document.createRange();  
-			range.selectNode(urlField);
-			window.getSelection().addRange(range);
-			document.execCommand('copy');				
-			window.getSelection().removeAllRanges();
-		};*/
-
 	}])	
 })();
