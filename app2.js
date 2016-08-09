5c9909cc01c90e6ff732cb3f3cb30245b9666796
Eduardo <edwardc94@outlook.com>
2016-08-08 22:58:50 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,1 +1,17 @@
-
+(function () {
+    var app = angular.module("NoteApplication", []);
+    
+    app.controller = angular.controller("GralCtrl" , ["$scope", function ($scope) {
+        function Policy(pN = "", phN = "", doP = "", depD = "", retD = "" ) {
+            this.pN = pN;
+            this.phN = phN;
+            this.doP = dop;
+            this.depD = depD;
+            this.retD = retD;
+        };
+        
+        $scope.data = {
+            
+        };
+    }])
+})();
