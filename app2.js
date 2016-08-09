ed79d98c1866524254b4444610acbf6a58b4ce4b
Eduardo <edwardc94@outlook.com>
2016-08-09 02:47:03 -0500

    Update app2.js

--- app2.js
+++ app2.js
@@ -2,16 +2,22 @@
     var app = angular.module("NoteApplication", []);
     
     app.controller = angular.controller("GralCtrl" , ["$scope", function ($scope) {
-        function Policy(pN = "", phN = "", doP = "", depD = "", retD = "" ) {
+        function Policy(pN = "", phN = "", doP = "", depD = "", retD = "", insureds = [{name : "", email : "", phone : "", address : ""}]) {
             this.pN = pN;
-            this.phN = phN;
             this.doP = dop;
             this.depD = depD;
             this.retD = retD;
+            this.insureds = insureds;
         };
-        
+        function FNOL(filedD = "", incD = "", benInfo = [{claimN : "", benefit : "", reason : "", amountClaimed : ""}]) {
+            this.filedD = filedD;
+            this.incD = incD;
+            this.benInfo = benInfo;
+        };
         $scope.data = {
-            
+            callType = [{id: 0, name : "FNOL"}, {id : 1, name : "Cancellation"}, {id : 2, name : "General Inquiry"}],
+            policy : new Policy(),
+            fnol : new FNOL()
         };
     }])
 })();
